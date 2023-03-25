7130c5b72dbf81833b7eb133ade4c2dbeaebb763
peethamber <40764612+peethamber@users.noreply.github.com>
2023-03-25 13:07:43 +0000

    Add files via upload

--- main2.py
+++ main2.py
@@ -209,9 +209,9 @@ def recommend(features,feature_list):
 # st.write(sys.argv[1])
 img_width = 230
 
-display_image = Image.open(img_name['img_name'][0])
+display_image = Image.open(img_name['img_name'])
 st.image(display_image,width=450)
-features = feature_extraction(os.path.join('./',img_name['img_name'][0]),model)
+features = feature_extraction(os.path.join('./',img_name['img_name']),model)
 indices = recommend(features,feature_list)
 
 st.subheader("Design recommendations from us")
